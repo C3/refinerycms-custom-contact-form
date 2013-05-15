7d6aa5f5c30ee8a9dceec23456a7f78ff9983c1c
Kate Deutscher <kate.deutscher@c3businesssolutions.com>
2013-05-15 11:13:30 +1000

    Fix incorrect duplicate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,5 +44,5 @@
 
   Refinery::Setting.find_or_set(:contact_form_from_address, 'no-reply@domain.com')
 
-  Refinery::Setting.find_or_set(:contact_form_from_address, 'no-reply@domain.com')
+  Refinery::Setting.find_or_set(:contact_form_reply_to_address, 'no-reply@domain.com')
 end
